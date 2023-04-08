cec746453e05c48b9f8de4e82471b460558c18ce
⎠⎠ THE༒KILLER ⎝⎝ <67904798+killerlearner@users.noreply.github.com>
2023-04-08 11:48:35 +0530

    Update update.py

--- update.py
+++ update.py
@@ -48,10 +48,11 @@ if UPDATE_PACKAGES.lower() == 'true':
 
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
 if len(UPSTREAM_REPO) == 0:
-   UPSTREAM_REPO = None
+   UPSTREAM_REPO = 'https://github.com/killerlearner/mltb'
 
-UPSTREAM_REPO = 'https://github.com/killerlearner/mltb'
-UPSTREAM_BRANCH = 'master'
+UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
+if len(UPSTREAM_BRANCH) == 0:
+   UPSTREAM_BRANCH = 'master'
 
 if UPSTREAM_REPO is not None:
     if ospath.exists('.git'):
